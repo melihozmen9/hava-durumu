12bcf3223c15ca3bd06648a972f6bbe80e218ff9
Melih Özmen <melihozmen9@gmail.com>
2022-08-05 23:32:55 +0300

    havadurumuzimir
    ilk düzenleme

--- hava durumu/ViewController.swift
+++ hava durumu/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Havasicakligi: UILabel!
+    @IBOutlet weak var hissedilen: UILabel!
+    @IBOutlet weak var ruzgar: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Weather API"
+        
     }
-
-
+   // 1. ADım WEbsitesine gideceğiz. istek göndereceğiz.
+    // 2. adım Datayı Alacagız.
+   // 3. ADım işlemek
+    @IBAction func ogrenbutonu(_ sender: Any) {
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=izmir&appid=e05a8069baa54e66a78bab788129da43)")
+        let session = URLSession.shared
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil {
+                print("error")
+            } else {
+                // 2. adım datayı aldık
+                
+                if data != nil {
+                    do {
+                        try <#throwing expression#>
+                    } catch <#pattern#> {
+                        <#statements#>
+                    }
+                    
+                }
+            }
+        }
+        task.resume()
+        
+        
+    }
+    
 }
 
